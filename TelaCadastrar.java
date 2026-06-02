57c1b1ab0f7cee64943556976b47b47f3749a8fd
Murilo Souza Targa <targasouza1@gmail.com>
2026-06-02 19:59:08 -0300

    Refactor TelaCadastrar for improved readability
    mensagem de erro melhorada para indicar exatamente o que está errado

--- TelaCadastrar.java
+++ TelaCadastrar.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class TelaCadastrar {
-    public TelaCadastrar(ArrayList<Livro> livros, ArrayList<Leitor> leitores, DefaultTableModel modeloTabela,
-            String[] modoAtual) {
+    public TelaCadastrar(ArrayList<Livro> livros, ArrayList<Leitor> leitores,
+            DefaultTableModel modeloTabela, String[] modoAtual) {
 
         if (modoAtual[0].equals("Livro")) {
             JFrame frameCadastro = new JFrame();
@@ -18,31 +18,21 @@ public class TelaCadastrar {
 
             JLabel labelNome = new JLabel("Nome Do Livro:");
             JTextField campoNome = new JTextField();
-
             JLabel labelAutor = new JLabel("Autor Do Livro:");
             JTextField campoAutor = new JTextField();
-
             JLabel labelPreco = new JLabel("Preço Do Livro:");
             JTextField campoPreco = new JTextField();
-
-            JLabel labelPaginas = new JLabel("Numero De Páginas Do Livro:");
+            JLabel labelPaginas = new JLabel("Numero De Páginas:");
             JTextField campoPaginas = new JTextField();
-
             JLabel labelGenero = new JLabel("Gênero Do Livro:");
             JTextField campoGenero = new JTextField();
-
             JButton botaoCadastrar = new JButton("Cadastrar");
 
-            painel.add(labelNome);
-            painel.add(campoNome);
-            painel.add(labelAutor);
-            painel.add(campoAutor);
-            painel.add(labelPreco);
-            painel.add(campoPreco);
-            painel.add(labelPaginas);
-            painel.add(campoPaginas);
-            painel.add(labelGenero);
-            painel.add(campoGenero);
+            painel.add(labelNome); painel.add(campoNome);
+            painel.add(labelAutor); painel.add(campoAutor);
+            painel.add(labelPreco); painel.add(campoPreco);
+            painel.add(labelPaginas); painel.add(campoPaginas);
+            painel.add(labelGenero); painel.add(campoGenero);
 
             frameCadastro.add(painel);
             frameCadastro.add(botaoCadastrar, BorderLayout.SOUTH);
@@ -52,55 +42,46 @@ public class TelaCadastrar {
                 if (campoNome.getText().isEmpty() || campoAutor.getText().isEmpty() ||
                         campoPreco.getText().isEmpty() || campoPaginas.getText().isEmpty() ||
                         campoGenero.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Preencha todos os campos!!");
+                    JOptionPane.showMessageDialog(null, "Preencha todos os campos!");
                     return;
                 }
-
                 try {
                     String nome = campoNome.getText();
                     String autor = campoAutor.getText();
                     double preco = Double.parseDouble(campoPreco.getText());
-                    int pagina = Integer.parseInt(campoPaginas.getText());
+                    int paginas = Integer.parseInt(campoPaginas.getText());
                     String genero = campoGenero.getText();
 
-                    livros.add(new Livro(nome, autor, preco, pagina, genero));
-                    JOptionPane.showMessageDialog(null, "O livro '" + nome + "' foi adicionado!");
+                    livros.add(new Livro(nome, autor, preco, paginas, genero));
+                    JOptionPane.showMessageDialog(null, "Livro '" + nome + "' cadastrado!");
                     TelaAdmin.renderizarTabela(livros, leitores, modeloTabela, modoAtual);
                     frameCadastro.dispose();
-
                 } catch (Exception erro) {
-                    JOptionPane.showMessageDialog(null, "Dados inseridos de forma incorreta!!");
+                    JOptionPane.showMessageDialog(null, "Preço e Páginas devem ser números!");
                 }
-
             });
         }
 
         if (modoAtual[0].equals("Leitor")) {
             JFrame frameCadastro = new JFrame();
-            frameCadastro.setTitle("Cadastrar leitor");
+            frameCadastro.setTitle("Cadastrar Leitor");
             frameCadastro.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frameCadastro.setLocationRelativeTo(null);
             frameCadastro.setSize(400, 150);
 
             JPanel painelCadastro = new JPanel(new GridLayout(3, 2, 10, 10));
 
-            JLabel labelNome = new JLabel("Insira o Nome do Novo Leitor");
+            JLabel labelNome = new JLabel("Nome do Novo Leitor:");
             JTextField campoNome = new JTextField(20);
-
-            JLabel labelTelefone = new JLabel("Insira o Telefone do Novo Leitor");
+            JLabel labelTelefone = new JLabel("Telefone do Novo Leitor:");
             JTextField campoTelefone = new JTextField(20);
-
-            JLabel labelSenha = new JLabel("Insira o Senha do Novo Leitor");
-            JTextField campoSenha = new JTextField(20);
-
+            JLabel labelSenha = new JLabel("Senha do Novo Leitor:");
+            JPasswordField campoSenha = new JPasswordField(20);
             JButton botaoCadastrar = new JButton("Cadastrar Leitor");
 
-            painelCadastro.add(labelNome);
-            painelCadastro.add(campoNome);
-            painelCadastro.add(labelTelefone);
-            painelCadastro.add(campoTelefone);
-            painelCadastro.add(labelSenha);
-            painelCadastro.add(campoSenha);
+            painelCadastro.add(labelNome); painelCadastro.add(campoNome);
+            painelCadastro.add(labelTelefone); painelCadastro.add(campoTelefone);
+            painelCadastro.add(labelSenha); painelCadastro.add(campoSenha);
 
             frameCadastro.add(painelCadastro);
             frameCadastro.add(botaoCadastrar, BorderLayout.SOUTH);
@@ -108,27 +89,23 @@ public class TelaCadastrar {
 
             botaoCadastrar.addActionListener(e -> {
                 if (campoNome.getText().isEmpty() || campoTelefone.getText().isEmpty()
-                        || campoSenha.getText().isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Preencha todos os campos!!");
+                        || new String(campoSenha.getPassword()).isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Preencha todos os campos!");
                     return;
                 }
-
                 try {
                     String nome = campoNome.getText();
                     String telefone = campoTelefone.getText();
-                    String senha = campoSenha.getText();
+                    String senha = new String(campoSenha.getPassword());
 
                     leitores.add(new Leitor(nome, telefone, senha));
-                    JOptionPane.showMessageDialog(null, "O leitor '" + nome + "' foi adicionado!");
+                    JOptionPane.showMessageDialog(null, "Leitor '" + nome + "' cadastrado!");
                     TelaAdmin.renderizarTabela(livros, leitores, modeloTabela, modoAtual);
                     frameCadastro.dispose();
-
-                } catch (Exception error) {
-                    JOptionPane.showMessageDialog(null, "Dados inseridos de forma incorreta!!");
+                } catch (Exception erro) {
+                    JOptionPane.showMessageDialog(null, "Dados inseridos de forma incorreta!");
                 }
             });
-
         }
-
     }
 }
